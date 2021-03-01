b0fa9e28250cbc34439c8ecd0ed780669d7fdc04
Joseph Bermudez <josephbermudez13@gmail.com>
2021-02-28 23:16:44 -0500

    Update process.java

--- process.java
+++ process.java
@@ -1,4 +1,5 @@
 import java.text.DecimalFormat;
+import java.util.Random;
 
 public class process
 {
@@ -11,26 +12,78 @@ public class process
     int processNum;
     int active;
     int arrivalTime;
-    int totalCPUTimes;
-    int RemainingCPUTimes;
+    double totalCPUTimes;
+    double RemainingCPUTimes;
     int turnAroundTime;
     int priorityLevel;
+    int timeInterval;
+    int currentTime = 0;
 
+    Random rand = new Random();
     DecimalFormat numberFormatter = new DecimalFormat("#.00");
 
-    public process(int PN, int A, int AT, int TCT,
-    int RCT, int PL)
+    public process(int PN, int A, int PL)
     {
         processNum = PN;
         active = A;
-        arrivalTime = AT;
-        totalCPUTimes = TCT;
-        RemainingCPUTimes = RCT;
         priorityLevel = PL;
     }
 
-    int[][] mainArray = new int[processNum][7];
+    double[][] mainArray = new double[processNum][7];
 
     //look at pseudocode
 
+    public void setActive()
+    {
+        for(int i = 0; i < processNum; i++)
+        {
+            mainArray[i][indActive] = active;
+        }
+    }
+
+    public void setTimeInterval(int k)
+    {
+        timeInterval = k;
+    }
+
+    public void setArrivalTime()
+    {
+        for (int i = 0; i < processNum;i++) 
+        {
+          arrivalTime = rand.nextInt(timeInterval);
+          mainArray[i][indArrivalTime] = arrivalTime;
+          setTurnAroundTime(currentTime, arrivalTime);
+        }
+    }
+
+    public void setCPUTime(int d, int v)
+    {
+        for (int i = 0; i < processNum; i++) 
+        {
+            double cpuTime = rand.nextGaussian() *d+v;
+            numberFormatter.format(cpuTime);
+
+            RemainingCPUTimes = cpuTime;
+            mainArray[i][indTotalCPUTime] = cpuTime;
+            mainArray[i][indRemainingCPUTime] = RemainingCPUTimes;
+        }
+    }
+
+    public void setTurnAroundTime(int currentT, int arrivalTime)
+  {
+    for (int i = 0; i < processNum; i++) 
+    {
+      int turnAround = currentT - arrivalTime;
+      mainArray[i][indTurnAroundTime] = turnAround;
+    }
+  }
+
+    public void setPriorityLevel()
+    {
+        for (int i = 0; i < processNum; i++) 
+        {
+          priorityLevel = rand.nextInt(10) + 1;
+          mainArray[i][indPriorityLevel] = priorityLevel;
+        }
+    }
 }
