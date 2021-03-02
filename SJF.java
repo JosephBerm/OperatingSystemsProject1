9ff9f8e2af10b3b5d606b5a071507e5ae35ba036
Joseph Bermudez <josephbermudez13@gmail.com>
2021-03-01 23:26:33 -0500

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -1,5 +1,30 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
 public class SJF
 {
-    
+    public static int indActive = 1; 
+    public static int indArrivalTime = 2;
+    public static int indTotalCPUTime = 3;
+    public static int indRemainingCPUTime = 4;
+    public static int indTurnAroundTime = 5;
+    public static int indPriorityLevel = 6;
+
+
+    public static double algo(double [][] arr, int n)
+    {
+        Arrays.sort(arr, new Comparator<double[]>()
+        {
+            @Override
+            public int compare(double[] o1, double[] o2)
+            {
+                double diff = o1[4] - o2[4];
+                return diff < 0 ? - 1 : diff == 0 ? 0 : 1;
+            }
+            
+        });
+        return arr[n][indRemainingCPUTime];
+    }
 }
 
