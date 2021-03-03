771cf560f17984e5e3a16c08b76a0df52eb3ab2e
Joseph Bermudez <josephbermudez13@gmail.com>
2021-03-02 23:01:08 -0500

    Update FIFO.java

--- FIFO.java
+++ FIFO.java
@@ -11,26 +11,23 @@ public class FIFO
     public static int indRemainingCPUTime = 4;
     public static int indTurnAroundTime = 5;
     public static int indPriorityLevel = 6;
-
+    static SortingArray sort = new SortingArray();
 
     public static double algo(double [][] arr, int n)
     {
-        Arrays.sort(arr, new Comparator<double[]>()
-        {
-            @Override
-            public int compare(double[] o1, double[] o2)
-            {
-                double diff = o1[2] - o2[2];
-                return diff < 0 ? - 1 : diff == 0 ? 0 : 1;
-            }
-            
-        });
+        arr = sort.sortArr(arr, 2);
         return arr[n][indRemainingCPUTime];
     }
 }
-/**/
 
-        /*int peek = pQueue.peek();
-        pQueue.poll();
-        return peek;*/
+//          Arrays.sort(arr, new Comparator<double[]>()
+//         {
+//             @Override
+//             public int compare(double[] o1, double[] o2)
+//             {
+//                 double diff = o1[2] - o2[2];
+//                 return diff < 0 ? - 1 : diff == 0 ? 0 : 1;
+//             }
+            
+//         });
         
