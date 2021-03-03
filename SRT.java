61ccc12ebcefd5c5cfa2064bf5d293878965b4a9
Joseph Bermudez <josephbermudez13@gmail.com>
2021-03-02 23:01:16 -0500

    Update SRT.java

--- SRT.java
+++ SRT.java
@@ -1,4 +1,33 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Arrays;
+
+
 public class SRT
 {
-    
+    public static int indActive = 1; 
+    public static int indArrivalTime = 2;
+    public static int indTotalCPUTime = 3;
+    public static int indRemainingCPUTime = 4;
+    public static int indTurnAroundTime = 5;
+    public static int indPriorityLevel = 6;
+    static SortingArray sort = new SortingArray();
+
+    public static double algoPrep(double [][] arr, int n)
+    {
+        arr = sort.sortArr(arr, 2);
+        
+        return arr[n][indRemainingCPUTime];
+    }
+
+//          Arrays.sort(arr, new Comparator<double[]>()
+//         {
+//             @Override
+//             public int compare(double[] o1, double[] o2)
+//             {
+//                 double diff = o1[2] - o2[2];
+//                 return diff < 0 ? - 1 : diff == 0 ? 0 : 1;
+//             }
+            
+//         });
 }
